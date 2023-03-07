375850af45e0afbcc26e2c502764360fc198562d
Sergey Shevchenko <sergiishevchenko017@gmail.com>
2023-03-07 08:52:10 +0100

    first commit for models

---
+++ movies/models.py
@@ -0,0 +1,78 @@
+from django.db import models
+from django.core.validators import MinValueValidator, MaxValueValidator
+from django.utils.translation import gettext_lazy as _
+
+from .mixins import UUIDMixin, TimeStampedMixin
+
+
+class Gender(models.TextChoices):
+    MALE = 'male', _('male')
+    FEMALE = 'female', _('female')
+
+
+class FilmTypes(models.TextChoices):
+    movie = 'movie', _('movie')
+    tv_show = 'tv_show', _('tv_show')
+
+
+class Genre(UUIDMixin, TimeStampedMixin):
+    name = models.CharField(_('name'), max_length=255)
+    description = models.TextField(_('description'), blank=True)
+
+    class Meta:
+        db_table = "content\".\"genre"
+        verbose_name = 'Жанр'
+        verbose_name_plural = 'Жанры'
+
+    def __str__(self):
+        return self.name
+
+
+class FilmWork(UUIDMixin, TimeStampedMixin):
+    title = models.CharField(_('title'), max_length=255)
+    description = models.TextField(_('description'), blank=True)
+    creation_date = models.DateField(_('creation_date'))
+    rating = models.FloatField(_('rating'), blank=True, validators=[MinValueValidator(0), MaxValueValidator(100)])
+    type = models.TextField(_('type'), choices=FilmTypes.choices)
+
+    genres = models.ManyToManyField(Genre, through='GenreFilmWork')
+
+    class Meta:
+        db_table = "content\".\"film_work"
+        verbose_name = 'Кинопроизведение'
+        verbose_name_plural = 'Кинопроизведения'
+
+    def __str__(self):
+        return self.title
+
+
+class GenreFilmWork(UUIDMixin):
+    film_work = models.ForeignKey(FilmWork, on_delete=models.CASCADE)
+    genre = models.ForeignKey(Genre, on_delete=models.CASCADE)
+    created = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        db_table = "content\".\"genre_film_work"
+
+
+class Person(UUIDMixin, TimeStampedMixin):
+    full_name = models.CharField(_('full_name'), max_length=255)
+    gender = models.TextField(_('gender'), choices=Gender.choices, null=True)
+
+    class Meta:
+        db_table = "content\".\"person"
+        verbose_name = 'Автор'
+        verbose_name_plural = 'Авторы'
+
+    def __str__(self):
+        return self.full_name
+
+
+class PersonFilmWork(UUIDMixin):
+    film_work = models.ForeignKey(FilmWork, on_delete=models.CASCADE)
+    person = models.ForeignKey(Person, on_delete=models.CASCADE)
+    role = models.TextField(_('role'), null=True)
+    created = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        db_table = "content\".\"person_film_work"
