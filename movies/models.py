6fbc87567eed755b5bb3606dc51ff15b88887653
Sergey Shevchenko <sergiishevchenko017@gmail.com>
2023-03-07 08:59:37 +0100

    initial migration

--- movies/models.py
+++ movies/models.py
@@ -5,11 +5,6 @@ from django.utils.translation import gettext_lazy as _
 from .mixins import UUIDMixin, TimeStampedMixin
 
 
-class Gender(models.TextChoices):
-    MALE = 'male', _('male')
-    FEMALE = 'female', _('female')
-
-
 class FilmTypes(models.TextChoices):
     movie = 'movie', _('movie')
     tv_show = 'tv_show', _('tv_show')
@@ -57,7 +52,6 @@ class GenreFilmWork(UUIDMixin):
 
 class Person(UUIDMixin, TimeStampedMixin):
     full_name = models.CharField(_('full_name'), max_length=255)
-    gender = models.TextField(_('gender'), choices=Gender.choices, null=True)
 
     class Meta:
         db_table = "content\".\"person"
