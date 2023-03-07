dd958f8e784283fad36f63b81a23e1ad420237fb
Sergey Shevchenko <sergiishevchenko017@gmail.com>
2023-03-07 08:51:06 +0100

    completed admin file

---
+++ movies/admin.py
@@ -0,0 +1,32 @@
+from django.contrib import admin
+from .models import Genre, FilmWork, GenreFilmWork, PersonFilmWork, Person
+
+
+@admin.register(Genre)
+class GenreAdmin(admin.ModelAdmin):
+    list_display = ('name', 'created', 'modified')
+    search_fields = ('name', 'description', 'id')
+
+
+class GenreFilmWorkInline(admin.TabularInline):
+    model = GenreFilmWork
+    autocomplete_fields = ('genre',)
+
+
+@admin.register(FilmWork)
+class FilmWorkAdmin(admin.ModelAdmin):
+    inlines = (GenreFilmWorkInline,)
+    list_display = ('title', 'type', 'creation_date', 'rating',)
+    list_filter = ('type',)
+    search_fields = ('title', 'description', 'id')
+
+
+class PersonFilmWorkInline(admin.TabularInline):
+    model = PersonFilmWork
+    autocomplete_fields = ('person',)
+
+
+@admin.register(Person)
+class PersonAdmin(admin.ModelAdmin):
+    inlines = (PersonFilmWorkInline,)
+    search_fields = ('full_name', 'id')
