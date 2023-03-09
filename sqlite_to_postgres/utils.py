30880d380a4d7de33779361ab4937f7d657e4c4e
Sergey Shevchenko <sergiishevchenko017@gmail.com>
2023-03-09 18:22:44 +0100

    fixed get_sqlite_data&get_postgres_data

--- sqlite_to_postgres/utils.py
+++ sqlite_to_postgres/utils.py
@@ -7,17 +7,17 @@ from models import FilmWork, Genre, GenreFilmWork, Person, PersonFilmWork
 def get_db_creds():
     load_dotenv()
     db_creds = {
-            'psql': {
-                'dbname': os.environ.get('DB_NAME'),
-                'user': os.environ.get('DB_USER'),
-                'password': os.environ.get('DB_PASSWORD'),
-                'host': os.environ.get('DB_HOST'),
-                'port': os.environ.get('DB_PORT')
-                },
-            'sqlite': {
-                'db_path': os.environ.get('sqlite_DB_NAME')
-                }
-            }
+        'psql': {
+            'dbname': os.environ.get('DB_NAME'),
+            'user': os.environ.get('DB_USER'),
+            'password': os.environ.get('DB_PASSWORD'),
+            'host': os.environ.get('DB_HOST'),
+            'port': os.environ.get('DB_PORT')
+        },
+        'sqlite': {
+            'db_path': os.environ.get('sqlite_DB_NAME')
+        }
+    }
     return db_creds
 
 
@@ -30,3 +30,17 @@ def get_models():
         'person_film_work': PersonFilmWork
     }
     return models
+
+
+def get_sqlite_data():
+    test_sqlite_data = {'created_at': 'substr(created_at,0,20)',
+                        'updated_at': 'substr(updated_at,0,20)'
+                        }
+    return test_sqlite_data
+
+
+def get_postgres_data():
+    test_postgres_data = {'created_at': 'TO_CHAR(created,\'YYYY-MM-DD HH24:MI:SS\')',
+                          'updated_at': 'TO_CHAR(modified,\'YYYY-MM-DD HH24:MI:SS\')',
+                          }
+    return test_postgres_data
