463b6b7c8b4809922ec8aa45dfd111ff047cc7f3
Sergey Shevchenko <sergiishevchenko017@gmail.com>
2023-03-10 09:45:56 +0100

    refactored ALLOWED_HOSTS

--- movies_admin/config/settings.py
+++ movies_admin/config/settings.py
@@ -20,7 +20,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 
 DEBUG = os.environ.get('DEBUG')
 
-ALLOWED_HOSTS = ['127.0.0.1']
+ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', '127.0.0.1').split(' ')
 
 if DEBUG:
     import socket
